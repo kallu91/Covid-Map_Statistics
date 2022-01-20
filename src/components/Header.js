4371594443dde02c23f6425228c8cebd5e76805f
kalyan <kalyankumar2020p@gmail.com>
2022-01-20 09:02:14 +0530

    first-commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <div className="navbar">
+        <h1>Coronavirus Map Statistics</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
